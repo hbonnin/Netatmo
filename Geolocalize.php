b965f1a44593f3db397a85cfe5b99ff16f152a86
hbonnin <fraysseix@free.fr>
2013-01-27 18:14:10 +0100

    return "BAD" if the google api has received too many queries g

--- Geolocalize.php
+++ Geolocalize.php
@@ -5,11 +5,12 @@ function geolocalize($lat,$lng)
     $ch = curl_init();
 	curl_setopt($ch, CURLOPT_URL, "$url");
     curl_setopt($ch, CURLOPT_HEADER, 0);
-    curl_setopt($ch,CURLOPT_CONNECTTIMEOUT,10);
+    curl_setopt($ch,CURLOPT_CONNECTTIMEOUT,15);
     curl_setopt($ch,CURLOPT_RETURNTRANSFER ,TRUE);
 	$result = curl_exec($ch);
     curl_close($ch);
     $decode = json_decode($result, TRUE);
+    if($decode['status'] != "OK")return "BAD";
     return explode(",", $decode['results'][0]['formatted_address']);
 	} 
 ?>	
