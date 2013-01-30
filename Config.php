0ba64b1ea8f2435301aa36a4c587294566d31f0d
hbonnin <fraysseix@free.fr>
2013-01-30 19:38:12 +0100

    Possibility to enter a google map API key

--- Config.php
+++ Config.php
@@ -1,6 +1,9 @@
 <?php
-$client_id = "CLIENT ID";
+$client_id = "CLIENT_ID";
 $client_secret = "CLIENT_SECRET";
-$test_username = "EMAL;
+$test_username = "EMAIL";
 $test_password = "PASSWORD";
+$google_key = "GOOGLE_KEY";
+$use_google_key = 0;  //1 to use the google key
+
 ?>
