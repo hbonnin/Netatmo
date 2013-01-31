57e460527ca0551d697f3e4705ae8d3fead80a20
hbonnin <fraysseix@free.fr>
2013-01-31 18:11:36 +0100

    Affiche toute les dernières mesures

--- google.php
+++ google.php
@@ -35,12 +35,14 @@ for($i = 0;$i < $numStations;$i++)
 	{$latitude[$i] = $devicelist["devices"][$i]["place"]["location"][1];
     $longitude[$i] = $devicelist["devices"][$i]["place"]["location"][0];
     $res = $mesures[$i]["modules"];
-    $txt = sprintf("%3.1f° %d%% %dmb",$res[1]['Temperature'],$res[1]['Humidity'],$res[0]['Pressure']);
-    $label[$i] = $txt;
+    $alt = $devicelist["devices"][$i]["place"]["altitude"];
+    $name = $mesures[$i]['station_name'] . ' (' . $alt . 'm)';
+    $txtEXT = sprintf("Ext: %3.1f° %d%% %dmb",$res[1]['Temperature'],$res[1]['Humidity'],$res[0]['Pressure']);
+	$txtINT = sprintf("Int: %3.1f° %d%% %dppm %ddb",$res[0]['Temperature'],$res[0]['Humidity']
+			,$res[0]['CO2'],$res[0]['Noise']);
+    $label[$i] = '<b>' . $name . '</b><br>' . $txtEXT . '<br>' . $txtINT;
+    //$label[$i] =  $txtEXT . '<br>' . $textINT;
 	}	
-
-
-
 
 echo("
 <!DOCTYPE html>
@@ -52,7 +54,7 @@ echo("
     <style type='text/css'>
       html { height: 100% }
       body { height: 100%; margin: 0; padding: 0 }
-      #map_canvas { height: 100% }
+      <!--#map_canvas { height: 100%;  }-->
     </style>
     <script type='text/javascript'
 ");    
@@ -62,14 +64,22 @@ echo("
 		echo("src='https://maps.googleapis.com/maps/api/js?&sensor=false'>");
 echo("
     </script>
-    <script type='text/javascript' src='StyledMarker.js'></script>
+    <!--<script type='text/javascript' src='StyledMarker.js'></script>-->
     <script type='text/javascript'>
     
 	function createMarker(pos,label,map) 
-	    {var marker = new StyledMarker({styleIcon:new StyledIcon(StyledIconTypes.BUBBLE,{color:'00ff00',text:label}),position:pos,map:map});
-		marker.setZIndex(100);
+	    {//var marker = new StyledMarker({styleIcon:new StyledIcon(StyledIconTypes.BUBBLE,{color:'00ff00',text:label}),position:pos,map:map});
+		var marker = new google.maps.Marker({'position':pos ,'map':map });
+		marker.setZIndex(103);
+		var infowindow = new google.maps.InfoWindow({'content'  : label});
 	   	google.maps.event.addListener(marker, 'click', function() 
-       		{marker.setZIndex(marker.getZIndex()-1);});    	 
+       		{marker.setZIndex(marker.getZIndex()-1);
+       		//alert('index:'+ marker.getZIndex());
+       		//infowindow.open(map, marker);
+       		});  
+       google.maps.event.addListener(marker, 'mouseover', function(){infowindow.open(map, marker);});
+       google.maps.event.addListener(marker, 'mouseout', function(){infowindow.close(map, marker);});
+         	 
     	return marker;  
 		}
 
@@ -112,7 +122,12 @@ echo("
     </script>
   </head>
   <body onload='initialize()'>
-  <div id='map_canvas' style='width: 640px; height: 100%; '></div>
+  <div style='width: 640px; height: 3%; position: relative; left: 20%;'> 
+  <i>Déplacer la souris sur un marqueur pour voir les informations</i>
+  </div>	
+  
+  <div id='map_canvas' style='width: 60%; height: 94%; left: 20%; border:solid 3px black;'> 
+  	</div>
   </body>
 </html>
 ");
