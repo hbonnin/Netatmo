1e73df646794cd9068dd395d37576be055dcf3a5
hbonnin <fraysseix@free.fr>
2013-06-27 16:54:38 +0200

    empty username and password

--- Config.php
+++ Config.php
@@ -1,6 +1,8 @@
 <?php
 $client_id = "CLIENT_ID";
 $client_secret = "CLIENT_SECRET";
+$test_username= ""; // Netatmo username
+$test_password= ""; // Netatmo password 
 $google_key = "GOOGLE_KEY";
 $use_google_key = 0;  //1 to use the google key
 ?>
