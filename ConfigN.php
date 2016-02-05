5a496b5a648c7a51558eee9f2bd6c4b9eaba52eb
hbonnin <fraysseix@free.fr>
2016-02-05 12:29:03 +0100

    delete

---
+++ ConfigN.php
@@ -0,0 +1,12 @@
+<?php
+$client_id = "";
+$client_secret = "";
+$google_key = "";
+$use_google_key = 1;
+$geocode = 1;
+$test_username= "";
+$test_password= "";
+$timezone = "Europe/Paris";
+$refreshInterval = 5*60; // refresh interval in seconds (0 no refresh) 
+?>
+
